0d952bb3452fa00e6659629d997317326477ba54
KarolMalinowski0605 <126602802+KarolMalinowski0605@users.noreply.github.com>
2023-08-12 01:20:18 +0200

    zad 2-SimpleLightbox library

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryContainer = document.querySelector('.gallery');
+
+function creatingGallery() {
+  const addHtml = galleryItems.map(item =>
+    `<div class="gallery__item">
+      <a class="gallery__link" href="${item.original}">
+        <img
+          class="gallery__image"
+          src="${item.preview}"
+          alt="${item.description}"
+        />
+      </a>
+    </div>`
+  ).join('');
+
+  galleryContainer.innerHTML = addHtml;
+}
+
+creatingGallery();
+
+const lightbox = new SimpleLightbox('.gallery__item a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
